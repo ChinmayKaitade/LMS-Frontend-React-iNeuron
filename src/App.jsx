aa2d3e96d4d4ab5f5570ce9a78178e17d1c9b517
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-08-06 08:07:23 +0530

    404 Error Page Not Found Page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 
 import AboutUs from "./Pages/AboutUs";
 import HomePage from "./Pages/HomePage";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
+
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
   );
