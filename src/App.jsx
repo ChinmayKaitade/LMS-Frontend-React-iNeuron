eefc4be6cff6c10de9da7a1c9a678426558ff2ba
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-08-12 14:30:20 +0530

    Course Details Component added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 
 import AboutUs from "./Pages/AboutUs";
 import Contact from "./Pages/Contact";
+import CourseDescription from "./Pages/Course/CourseDescription";
 import CourseList from "./Pages/Course/CourseList";
 import Denied from "./Pages/Denied";
 import HomePage from "./Pages/HomePage";
@@ -20,6 +21,11 @@ function App() {
         <Route path="/courses" element={<CourseList />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/denied" element={<Denied />}></Route>
+
+        <Route
+          path="/course/description"
+          element={<CourseDescription />}
+        ></Route>
 
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/login" element={<Login />}></Route>
